1dc63266a51db08547d4b2d58eaf002e1b397c89
GubkodevPY <gubkodev@gmail.com>
2024-05-29 20:32:13 +0300

    Новая реализация

---
+++ config.py
@@ -0,0 +1,15 @@
+# https://t.me/HelpCergik_bot
+# Домашний помощник
+TOKEN = '6446089490:AAHCJV6Vs3P84NVMBe4ZYSG2g5q1fB9ft_o' # токен доступа
+continuous_processing = True # настройка бота на постоянное выполнение
+
+message_start = "Даров, че надо?"
+message_command_purchases = 'товары' or 'Товары' or 'покупки' or 'Покупки'
+message_no_need_to_buy_anything = "Ничего на надо, не сливай бабки, транжира"
+
+# Подключение к базе данных MySQL
+# conn = mysql.connector.connect(
+#    host="localhost",
+#    user="username",
+#    password="password",
+#    database="mydb"
